8927c2aa9d94cf853711c43e7383bb0b2bb30b99
Mekbib-awono <mekbib.awono@gmail.com>
2020-06-08 11:42:11 +0300

    added audio files to Nahom's fork

--- teach.js
+++ teach.js
@@ -1,24 +1,50 @@
 //alert("HELLO FROM APP.JS!");
-var pics = [
-        "img/zoe.jpg",
-        "img/square.jpg",
-        "img/rectangle.jpg",
-        "img/triangle.jpg",
-        "img/circle.jpg",
-        "img/diamond.jpg",
-        "img/hexagon.jpg"
-        
-   ];
+var shapes = [
+    {
+        img: 'img/zoe.jpg',
+        audio: 'audio/zoe.ogg'
+    },
+    {
+        img: 'img/square.jpg',
+        audio: 'audio/square.ogg'
+    },
+    {
+        img: 'img/rectangle.jpg',
+        audio: 'audio/rectangle.ogg'
+    },
+    {
+        img: 'img/triangle.jpg',
+        audio: 'audio/triangle.ogg'
+    },
+    {
+        img: 'img/circle.jpg',
+        audio: 'audio/circle.ogg'
+    },
+    {
+        img: 'img/diamond.jpg',
+        audio: 'audio/diamond.ogg'
+    },
+    {
+        img: 'img/hexagon.jpg',
+        audio: 'audio/hexagon.ogg'
+    },
+]
 
 var zozo = document.querySelector("button");
 var img = document.querySelector("img"); 
+var audioSource = document.querySelector("audio > source")
+var audio = document.querySelector("audio")
+
+
 var counter = 1;
 
 zozo.addEventListener("click", function(){
-	if(counter === 7){
-		 counter = 0;
+    if(counter === 7){
+         counter = 0;
     }
 
-	img.src = pics[counter]
+    img.src = shapes[counter].img;
+    audioSource.src = shapes[counter].audio;
+    audio.load();
     counter = counter + 1;  
 });
